03678377e1dd8999a3f1071d60d376a432ab377a
Nicolas Santos Toledo <nicolas01.toledo@gmail.com>
2025-04-30 10:38:23 -0300

    correção: ajuste na URL da homepage e no caminho base do Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portif-lio_Toledo',
+  base: '/Portifolio_Toledo',
 })
